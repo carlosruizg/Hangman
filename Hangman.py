7398963dfba98a72b63b2d393741e85a08bb8512
charliewr <carlosrg787@gmail.com>
2017-04-12 10:13:41 +0100

    hangman first version with captain and refactor

---
+++ Hangman.py
@@ -0,0 +1,49 @@
+
+class Hangman:
+    
+    def __init__(self, word):
+        self.word = word
+        self.chances = 7
+        self.remaining = ['_'] * len(self.word)
+        self.usedletters = []
+        self.wordFound = False
+        self._play()
+    
+    def _right_guess(self, letter):        
+        print('Correct!')
+        indexes = [ind for (ind, char) in enumerate(self.word) if char == letter]
+        for c in indexes:
+            self.remaining[c] = letter                     
+        print(''.join(self.remaining))        
+        if ''.join(self.remaining) == self.word:
+            self.wordFound = True
+    
+    def _wrong_guess(self, letter):
+        print('Wrong!')
+        self.chances-=1
+    
+    def _guess(self, letter):
+        if letter in self.usedletters:
+            print('Repeated letter!')
+        else:
+            self.usedletters.append(letter)
+            if letter in self.word:
+                self._right_guess(letter)
+            else:
+                self._wrong_guess(letter)
+                    
+    def _play(self):
+        print('Welcome to Hangman!')  
+        while(self.chances > 0 and self.wordFound == False):
+            letter = input('Enter letter: ')
+            if letter:
+                self._guess(letter)
+                    
+        if self.wordFound:
+            print('You Win!')
+        else:
+            print('You lose!')
+    
+if __name__ == '__main__':
+    h = Hangman('captain')
+    
